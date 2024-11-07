96a05d96b884ec0e892fb4b83a1fb5c67527254b
Teimuraz Shengelia <teimurazshengelia@credo.ge>
2024-11-07 16:56:38 +0400

    Add project files.

---
+++ davaleba8/Program.cs
@@ -0,0 +1,25 @@
+﻿
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+
+    }
+} 
+class Program
+{ 
+    static void Main(string[] args)
+    {
+        string persoName = "giorgi";
+        int personAge = 56;
+        Console.WriteLine($"Name: {persoName}, Age: {personAge}");
+    }
+
+    
+}
